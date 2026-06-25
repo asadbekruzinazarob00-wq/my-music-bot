f0000866adfb2032f6956e7a7bd13189eb6003ed
asadbekruzinazarob00-wq <asadbekruzinazarob00@gmail.com>
2026-06-25 20:25:21 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+bot.py
+import os
+from pyrogram import Client, filters
+
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("BOT_TOKEN")
+
+app = Client("my_music_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text("Salom! Men ishlayapman va musiqalarni tayyorlayapman.")
+
+app.run()
